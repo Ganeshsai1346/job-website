8c1234cc9d3c95d370f73a5545584c7c4b116445
Ganesh sai <ganeshsai1346@gmail.com>
2022-11-17 17:41:25 +0100

    changes

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,6 +1,31 @@
 /** @format */
 
+import { useEffect, useState } from "react";
+
 const ContactUs = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetchProfiles();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const fetchProfiles = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/profiles");
+      if (response.ok) {
+        const profiles = await response.json();
+
+        console.log(profiles);
+        setList(profiles);
+        console.log("this is a new array", list);
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return <></>;
 };
 
