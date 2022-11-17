a8d2032a70c3e36a04acd78e76acd475663ff725
Ganesh sai <ganeshsai1346@gmail.com>
2022-11-17 17:37:29 +0100

    store

---
+++ src/redux/store/index.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import mainReducer from "./reducers/reducers.js";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: mainReducer,
+});
+
+export default store;
